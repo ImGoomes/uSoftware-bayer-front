7f9e2cf8faa145d9ff692fee7b53ac94dd54736a
AlmeidaigorCarlos <almeidaigorcarlos@gmail.com>
2019-08-21 19:39:09 -0300

    nao considerar

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SignIn from './views/SignIn';
 import SignUp from './views/SignUp';
+import MainRecruiter from './views/MainRecruiter';
+import MainCandidate from './views/MainCandidate';
 import {Router, Switch, Route} from 'react-router-dom'
 import {createBrowserHistory} from 'history'
 import 'bootstrap/dist/css/bootstrap.css';
@@ -11,10 +13,28 @@ import 'bootstrap/dist/css/bootstrap.css';
 import dotenv from 'dotenv'
 dotenv.config()
 
+const renderPrivateComponent = _ => {
+    try {
+        let signInData = JSON.parse(localStorage.getItem('siginInData'))
+        
+        switch(signInData.role){
+            case 'recruiter':
+                return MainRecruiter
+            case 'user':
+                return MainCandidate
+            default:
+                return SignIn
+            }
+        } catch (error) {
+            return SignIn            
+    }
+}
+
 ReactDOM.render(<Router history={createBrowserHistory()}>
                     <Switch>
                         <Route path="/" component={SignIn} exact={true}/>
                         <Route path="/signin" component={SignIn} exact={true}/>
                         <Route path="/signup" component={SignUp} exact={true}/>
+                        <Route path="/main" component={renderPrivateComponent()} exact={true}/>
                     </Switch>
                 </Router>, document.getElementById('root'));
---
+++ src/views/MainCandidate.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+
+export default class MainCandidate extends React.Component{
+    constructor(props){
+        super(props)
+        
+    }
+
+    render(){
+        return (
+            "MainCandidate"
+        )
+    }
+
+    componentDidMount(){
+
+    }
+
+    componentWillUnmount(){
+
+    }
+}
