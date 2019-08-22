ad0e838f9d5d1bdde884f92b539b13e8870f5a70
Gabriel Gomes <gabrielsousagomes@hotmail.com>
2019-08-21 22:37:01 -0300

    component de menu lateral criado

--- src/views/MainRecruiter.jsx
+++ src/views/MainRecruiter.jsx
@@ -1,73 +1,16 @@
-import React, { Component } from "react";
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Typography from '@material-ui/core/Typography'
-import Button from '@material-ui/core/Button'
-import Drawer from '@material-ui/core/Drawer'
-import { makeStyles } from "@material-ui/styles";
-import IconButton from '@material-ui/core/IconButton'
-import MenuIcon from '@material-ui/icons/Menu'
-  
+import React from 'react'
+import MenuLateral from '../components/lateral-menu'
 
-export default class MainRecruiter extends React.Component{
-  constructor(props) {
-    super(props);
-        this.state = {
-          recruiterName: 'teste recrutador'
-        }
-      }
-      componentDidMount() {
-        
-      }
-      componentDidUpdate() {
-        
-      }
-      
-      useStyles(){
-        return (makeStyles(theme => ({
-          root: {
-            flexGrow: 1,
-          },
-          menuButton: {
-            marginRight: theme.spacing(2),
-          },
-          title: {
-            flexGrow: 1,
-          },
-        })))();
-    }
+export default function(){
 
-      
-      render(){
-        const classes = makeStyles(theme => ({
-          root: {
-            flexGrow: 1,
-          },
-          menuButton: {
-            marginRight: theme.spacing(2),
-          },
-          title: {
-            flexGrow: 1,
-          },
-        }))
+  return (
+      <div>
+        <MenuLateral />
+        <div style={{position:'absolute', top: '100px', left:'100px'}}>
 
-        alert(classes)
+          testestts
+        </div>
+        </div>
+  )
 
-        return (
-          <div className={classes.root}>
-            <AppBar position="static">
-              <Toolbar>
-                <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-                  <MenuIcon />
-                </IconButton>
-                <Typography variant="h6" className={classes.title}>
-                  News
-                </Typography>
-                <Button color="inherit">Login</Button>
-              </Toolbar>
-            </AppBar>
-          </div>
-
-        );
-      }
 }
