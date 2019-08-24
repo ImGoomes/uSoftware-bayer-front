76efbbca0f6797e2b51bd8ab9ca75510a4ad0e11
AlmeidaigorCarlos <almeidaigorcarlos@gmail.com>
2019-08-24 18:56:33 -0300

    volta para o bootstrap e refatoracao dos componentes de signin e signup

--- src/views/SignIn.jsx
+++ src/views/SignIn.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import './../css/signin.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
-
-export default class SignIn extends React.Component{
-    constructor(props){
+export default class SignIn extends React.Component {
+    constructor(props) {
         super(props)
         this.state = {
             email: '',
@@ -13,52 +13,50 @@ export default class SignIn extends React.Component{
         }
     }
 
-    render(){
+    render() {
         return (
-            <div className="container d-flex justify-content-center mt-5">
-                <div className="box width-signin">
-                    <div className="brand">
-                        <i className="pe-7s-portfolio"></i>
-                        <span className="nome-projeto text-danger">u.Job</span>
-                    </div>
-                    <form className="form">
-                        <label className="control-label">E-mail</label>
-                        <input type="text" className="form-control mb-2" id="email" placeholder="e-mail" value={this.state.email} onChange={this.setEmail}/>
-                        <label className="control-label">Password</label>
-                        <input type="password" className="form-control mb-2" id="password" placeholder="password" value={this.state.password} onChange={this.setPassword}/>
-                        <hr />
-                        <button type="button" className="btn btn-blue btn-block" onClick={this.signIn}>Entrar</button>
-                        <Link className="btn btn-blue btn-block" to="/signup">Registrar</Link>
-                    </form>
-                </div>
-            </div>
+
+            <Container>
+                <Row className="justify-content-center">
+                    <Col md="auto">
+                        <div className="box width-signin">
+                            <header className="brand">
+                                <i className="pe-7s-portfolio"></i>
+                                <span className="nome-projeto text-danger">u.Job</span>
+                            </header>
+                            <form className="form">
+                                <label className="control-label">E-mail</label>
+                                <input type="text" className="form-control mb-2" id="email" placeholder="e-mail" value={this.state.email} onChange={this.setEmail} />
+                                <label className="control-label">Password</label>
+                                <input type="password" className="form-control mb-2" id="password" placeholder="password" value={this.state.password} onChange={this.setPassword} />
+                                <hr />
+                                <button type="button" className="btn btn-blue btn-block" onClick={this.signIn}>Entrar</button>
+                                <Link className="btn btn-blue btn-block" to="/signup">Registrar</Link>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
+
         )
     }
 
-    componentDidMount(){
-
-    }
-
-    componentWillUnmount(){
-
-    }
-
     setEmail = props => this.setState({
-            email:props.target.value
-        }, ()=>console.log(`e-mail: ${this.state.email}`))
+        email: props.target.value
+    }, () => console.log(`e-mail: ${this.state.email}`))
 
     setPassword = props => this.setState({
-            password:props.target.value
-        }, ()=>console.log(`password: ${this.state.password}`))
+        password: props.target.value
+    }, () => console.log(`password: ${this.state.password}`))
 
     checkForm = _ => {
-        if(this.state.email === '')
+        if (this.state.email === '')
             throw new Error('Email é um campo obrigatório')
-        else if(this.state.password === '')
+        else if (this.state.password === '')
             throw new Error('Senha é um campo obrigatório')
-        
+
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        if(!re.test(String(this.state.email).toLowerCase()))
+        if (!re.test(String(this.state.email).toLowerCase()))
             throw new Error('O e-mail fornecido não é válido')
     }
 
@@ -67,10 +65,10 @@ export default class SignIn extends React.Component{
             //Verificação do formulário
             this.checkForm()
 
-            const signIn = await axios.post(`${process.env.REACT_APP_API_ADDRESS}/signin`, {...this.state})
+            const signIn = await axios.post(`${process.env.REACT_APP_API_ADDRESS}/signin`, { ...this.state })
             alert('Credenciais corretas. Seja bem-vindo')
 
-            localStorage.setItem('signin', JSON.stringify({token: signIn.data}))
+            localStorage.setItem('signin', JSON.stringify({ token: signIn.data }))
             this.props.history.push('/signup')//Alterar URL
 
         } catch (error) {
@@ -78,13 +76,13 @@ export default class SignIn extends React.Component{
             localStorage.removeItem('signin')
 
             //Se não é um erro de rede | Banco de dados ou back-end
-            if(error.response === undefined){
+            if (error.response === undefined) {
                 alert(error.message)
                 return
             }
 
             //Se é um erro da banco de dados ou back-end
-            switch(error.response.status){
+            switch (error.response.status) {
                 case 404:
                     alert('e-mail ou senha incorretos')
                     break;
--- src/views/SignUp.jsx
+++ src/views/SignUp.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import {Link} from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import InputMask from 'react-input-mask'
 import './../css/signin.css'
+import { Container, Row, Col } from 'react-bootstrap'
 
-export default class SignIn extends React.Component{
-    constructor(props){
+export default class SignIn extends React.Component {
+    constructor(props) {
         super(props)
-        
+
         this.state = {
             name: '',
             lastName: '',
-            age: 0,
+            birth: new Date(),
             address: '',
             mobilePhone: '',
             email: '',
@@ -20,109 +21,101 @@ export default class SignIn extends React.Component{
         }
     }
 
-    render(){
-        return(
-            <div className="container d-flex justify-content-center mt-5">
-                <div className="box width-signup">
-                    <form className="form">
-                        <h2>Registre-se</h2>
-                        <label className="control-label">Nome</label>
-                        <input type="text" className="form-control mb-2" id="name" placeholder="Nome" value={this.state.name} onChange={this.setName}/>
-                        <label className="control-label">Sobrenome</label>
-                        <input type="text" className="form-control mb-2" id="lastName" placeholder="Sobrenome" value={this.state.lastName} onChange={this.setLastName}/>
-                        <label className="control-label">Idade</label>
-                        <input type="number" className="form-control mb-2" id="age" placeholder="Idade" value={this.state.age} onChange={this.setAge}/>
-                        <label className="control-label">Telefone</label>
-                        <InputMask mask="(99) 99999-9999" className="form-control mb-2" maskChar=" " placeholder="Celular" value={this.state.mobilePhone} onChange={this.setMobilePhone}/>                    
-                        <label className="control-label">E-mail</label>
-                        <input type="email"  className="form-control mb-2" id="email" placeholder="E-mail" value={this.state.email} onChange={this.setEmail}/>
-                        <label className="control-label">Senha</label>
-                        <input type="password" className="form-control mb-2" id="password" placeholder="Senha" value={this.state.password} onChange={this.setPassword}/>
-                        <label className="control-label">Confirme a Senha</label>
-                        <input type="password" className="form-control mb-2" id="passwordConfirmation" placeholder="Confirmação de Senha" value={this.state.passwordConfirmation} onChange={this.setPasswordConfirmation}/>
-                        
-                        <button type="button" className="btn btn-blue btn-block" onClick={this.signUp}>Registrar</button>
-                        <Link className="btn btn-blue btn-block" to="/signin">Entrar</Link>
-                    </form>
-                </div>
-            </div>
+    render() {
+        return (
+            <Container>
+                <Row className="justify-content-center">
+                    <Col md="auto">
+                        <div className="box width-signup">
+                            <form className="form">
+                                <h2>Registre-se</h2>
+                                <label className="control-label">Nome</label>
+                                <input type="text" className="form-control mb-2" id="name" placeholder="Nome" value={this.state.name} onChange={this.setName} />
+                                
+                                <label className="control-label">Sobrenome</label>
+                                <input type="text" className="form-control mb-2" id="lastName" placeholder="Sobrenome" value={this.state.lastName} onChange={this.setLastName} />
+                                
+                                <label className="control-label">Telefone</label>
+                                <InputMask mask="(99) 99999-9999" className="form-control mb-2" maskChar=" " placeholder="Celular" value={this.state.mobilePhone} onChange={this.setMobilePhone} />
+                                
+                                <label className="control-label">E-mail</label>
+                                <input type="email" className="form-control mb-2" id="email" placeholder="E-mail" value={this.state.email} onChange={this.setEmail} />
+                                
+                                <label className="control-label">Senha</label>
+                                <input type="password" className="form-control mb-2" id="password" placeholder="Senha" value={this.state.password} onChange={this.setPassword} />
+                                
+                                <label className="control-label">Confirme a Senha</label>
+                                <input type="password" className="form-control mb-2" id="passwordConfirmation" placeholder="Confirmação de Senha" value={this.state.passwordConfirmation} onChange={this.setPasswordConfirmation} />
+
+                                <footer>
+                                    <button type="button" className="btn btn-blue btn-block" onClick={this.signUp}>Registrar</button>
+                                    <Link className="btn btn-blue btn-block" to="/signin">Entrar</Link>
+                                </footer>
+                            </form>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         )
     }
 
-    componentDidMount(){
-
-    }
-
-    componentWillUnmount(){
-
-    }
-
     setName = props => {
-        this.setState({name: props.target.value},
-            ()=>console.log(this.state))
+        this.setState({ name: props.target.value },
+            () => console.log(this.state))
     }
-    
-    setLastName = props => {
-        this.setState({lastName: props.target.value},
-            ()=>console.log(this.state))
-    }
-    
-    setAge = props =>{
-        const age = parseInt(props.target.value)
-        if(age < 0 || age > 100)
-            return
 
-        this.setState({age: age},
-            ()=>console.log(this.state))
+    setLastName = props => {
+        this.setState({ lastName: props.target.value },
+            () => console.log(this.state))
     }
-    
+
     setAddress = props => {
-        this.setState({address: props.target.value},
-            ()=>console.log(this.state))
+        this.setState({ address: props.target.value },
+            () => console.log(this.state))
     }
-    
+
     setMobilePhone = props => {
-        this.setState({mobilePhone: props.target.value},
-            ()=>console.log(this.state))
+        this.setState({ mobilePhone: props.target.value },
+            () => console.log(this.state))
     }
-    
-    setEmail = props =>{
-        this.setState({email: props.target.value},
-            ()=>console.log(this.state))
+
+    setEmail = props => {
+        this.setState({ email: props.target.value },
+            () => console.log(this.state))
     }
 
     setPassword = props => {
-        this.setState({password: props.target.value},
-            ()=>console.log(this.state))
+        this.setState({ password: props.target.value },
+            () => console.log(this.state))
     }
 
     setPasswordConfirmation = props => {
-        this.setState({passwordConfirmation: props.target.value},
-            ()=>console.log(this.state))
+        this.setState({ passwordConfirmation: props.target.value },
+            () => console.log(this.state))
     }
 
     checkForm = _ => {
-        if(this.state.name === '')
+        if (this.state.name === '')
             throw new Error('Nome é um campo obrigatório')
-        else if(this.state.lastName === '')
+        else if (this.state.lastName === '')
             throw new Error('Sobrenome é um campo obrigatório')
-        else if(this.state.age === 0)
-            throw new Error('Idade é um campo obrigatório')
+        // else if (this.state.age === 0)
+            // throw new Error('Idade é um campo obrigatório')
         // else if(this.state.address === '')
-            // throw new Error('Endereço é um campo obrigatório')
+        // throw new Error('Endereço é um campo obrigatório')
         // else if(this.state.mobilePhone === '')
         //     throw new Error('Celular é um campo obrigatório')
-        else if(this.state.email === '')
+        else if (this.state.email === '')
             throw new Error('Email é um campo obrigatório')
-         else if(this.state.password === '')
-             throw new Error('Senha é um campo obrigatório')
-        else if(this.state.passwordConfirmation === '')
+        else if (this.state.password === '')
+            throw new Error('Senha é um campo obrigatório')
+        else if (this.state.passwordConfirmation === '')
             throw new Error('Confimação de Senha é um campo obrigatório')
-        else if(this.state.password !== this.state.passwordConfirmation)
+        else if (this.state.password !== this.state.passwordConfirmation)
             throw new Error('A senha e sua confirmação devem ser examente iguais')
-        
+
         const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        if(!re.test(String(this.state.email).toLowerCase()))
+        if (!re.test(String(this.state.email).toLowerCase()))
             throw new Error('O e-mail fornecido não é válido')
     }
 
@@ -130,13 +123,13 @@ export default class SignIn extends React.Component{
         try {
             //Verificando se o formulário está preenchido corretamente
             this.checkForm()
-            
+
             //Chamada para o back-end
-            const signUp = await axios.post(`${process.env.REACT_APP_API_ADDRESS}/signup`, {...this.state})            
-            
+            const signUp = await axios.post(`${process.env.REACT_APP_API_ADDRESS}/signup`, { ...this.state })
+
             console.log(signUp)
             alert('Cadastro realizado com sucesso. Você será redirecionado para a tela de login')
-            
+
             //Alterando a URL sem dar o load na página
             this.props.history.push('/signin') //Alterar a URL
         } catch (error) {
